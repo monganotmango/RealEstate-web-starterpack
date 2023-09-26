d36cedfb3380577acad7b0377c64128bd3343414
Mithravinda Nambiar <91652656+monganotmango@users.noreply.github.com>
2023-09-26 15:20:43 +0530

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Companies from './components/Companies/Companies'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Residencies from './components/Residencies/Residencies'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Hero />
+      <Companies />
+      <Residencies />
     </div>
   )
 }
